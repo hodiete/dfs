2b41d61411d9b4da44c79068e555b5434f0b47eb
Lindsey Catlett <lindsey.catlett@gmail.com>
2016-03-15 07:28:48 -0400

    NDD-309: Added ACSF and ACSFdev drush aliases to support phing deploys.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -317,45 +317,233 @@
  * Some examples appear below.  Remove the leading hash signs to enable.
  */
 
-#$aliases['stage'] = array(
-#    'uri' => 'http://stage.example.com',
-#    'root' => '/path/to/remote/drupal/root',
-#    'remote-host' => 'mystagingserver.myisp.com',
-#    'remote-user' => 'publisher',
-#    'os' => 'Linux',
-#    'path-aliases' => array(
-#      '%drush' => '/path/to/drush',
-#      '%drush-script' => '/path/to/drush/drush',
-#      '%files' => 'sites/mydrupalsite.com/files',
-#      '%custom' => '/my/custom/path',
-#     ),
-#     'variables' => array(
-#        'site_name' => 'My Drupal site',
-#      ),
-#     'command-specific' => array (
-#       'sql-sync' => array (
-#         'no-dump' => TRUE,
-#       ),
-#     ),
-#     # This shell alias will run `mycommand` when executed via
-#     # `drush @stage site-specific-alias`
-#     'shell-aliases' => array (
-#       'site-specific-alias' => '!mycommand',
-#     ),
-#  );
-#$aliases['dev'] = array(
-#    'uri' => 'http://dev.example.com',
-#    'root' => '/path/to/drupal/root',
-#    'variables' => array(
-#      'mail_system' => array('default-system' => 'DevelMailLog'),
-#    ),
-#  );
-#$aliases['server'] = array(
-#    'remote-host' => 'mystagingserver.myisp.com',
-#    'remote-user' => 'publisher',
-#    'os' => 'Linux',
-#  );
-#$aliases['live'] = array(
-#    'uri' => 'http://example.com',
-#    'root' => $aliases['dev']['root'],
-#  ) + $aliases['server'];
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+
+// Site webny, environment local
+$aliases['webny.loc'] = array(
+  'root' => '/var/www/webny/docroot',
+  'uri' => 'webny.dev',
+  )
+);
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+
+// Site acsfdev, environment dev
+$aliases['dev'] = array(
+  'root' => '/var/www/html/acsfdev.dev/docroot',
+  'ac-site' => 'acsfdev',
+  'ac-env' => 'dev',
+  'ac-realm' => 'prod',
+  'uri' => 'acsfdevdev.prod.acquia-sites.com',
+  'remote-host' => 'staging-13081.prod.hosting.acquia.com',
+  'remote-user' => 'acsfdev.dev',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['dev.livedev'] = array(
+  'parent' => '@acsfdev.dev',
+  'root' => '/mnt/gfs/acsfdev.dev/livedev/docroot',
+);
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site acsfdev, environment prod
+$aliases['prod'] = array(
+  'root' => '/var/www/html/acsfdev.prod/docroot',
+  'ac-site' => 'acsfdev',
+  'ac-env' => 'prod',
+  'ac-realm' => 'prod',
+  'uri' => 'acsfdev.prod.acquia-sites.com',
+  'remote-host' => 'web-12383.prod.hosting.acquia.com',
+  'remote-user' => 'acsfdev.prod',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['prod.livedev'] = array(
+  'parent' => '@acsfdev.prod',
+  'root' => '/mnt/gfs/acsfdev.prod/livedev/docroot',
+);
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site acsfdev, environment ra
+$aliases['ra'] = array(
+  'root' => '/var/www/html/acsfdev.ra/docroot',
+  'ac-site' => 'acsfdev',
+  'ac-env' => 'ra',
+  'ac-realm' => 'prod',
+  'uri' => 'acsfdevra.prod.acquia-sites.com',
+  'remote-host' => 'staging-15594.prod.hosting.acquia.com',
+  'remote-user' => 'acsfdev.ra',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['ra.livedev'] = array(
+  'parent' => '@acsfdev.ra',
+  'root' => '/mnt/gfs/acsfdev.ra/livedev/docroot',
+);
+
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site acsfdev, environment test
+$aliases['test'] = array(
+  'root' => '/var/www/html/acsfdev.test/docroot',
+  'ac-site' => 'acsfdev',
+  'ac-env' => 'test',
+  'ac-realm' => 'prod',
+  'uri' => 'acsfdevstg.prod.acquia-sites.com',
+  'remote-host' => 'staging-13081.prod.hosting.acquia.com',
+  'remote-user' => 'acsfdev.test',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['test.livedev'] = array(
+  'parent' => '@acsfdev.test',
+  'root' => '/mnt/gfs/acsfdev.test/livedev/docroot',
+);
+// Site nysits, environment 01dev
+$aliases['01dev'] = array(
+  'root' => '/var/www/html/nysits.01dev/docroot',
+  'ac-site' => 'nysits',
+  'ac-env' => '01dev',
+  'ac-realm' => 'enterprise-g1',
+  'uri' => 'nysits01dev.enterprise-g1.acquia-sites.com',
+  'site' => 'nysits',
+  'env' => '01dev',
+  'remote-host' => 'staging-538.enterprise-g1.hosting.acquia.com',
+  'remote-user' => 'nysits.01dev',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['01dev.livedev'] = array(
+  'parent' => '@nysits.01dev',
+  'root' => '/mnt/gfs/nysits.01dev/livedev/docroot',
+);
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site nysits, environment 01devup
+$aliases['01devup'] = array(
+  'root' => '/var/www/html/nysits.01devup/docroot',
+  'ac-site' => 'nysits',
+  'ac-env' => '01devup',
+  'ac-realm' => 'enterprise-g1',
+  'uri' => 'nysits01devup.enterprise-g1.acquia-sites.com',
+  'site' => 'nysits',
+  'env' => '01devup',
+  'remote-host' => 'staging-538.enterprise-g1.hosting.acquia.com',
+  'remote-user' => 'nysits.01devup',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['01devup.livedev'] = array(
+  'parent' => '@nysits.01devup',
+  'root' => '/mnt/gfs/nysits.01devup/livedev/docroot',
+);
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site nysits, environment 01live
+$aliases['01live'] = array(
+  'root' => '/var/www/html/nysits.01live/docroot',
+  'ac-site' => 'nysits',
+  'ac-env' => '01live',
+  'ac-realm' => 'enterprise-g1',
+  'uri' => 'nysits01live.enterprise-g1.acquia-sites.com',
+  'site' => 'nysits',
+  'env' => '01live',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['01live.livedev'] = array(
+  'parent' => '@nysits.01live',
+  'root' => '/mnt/gfs/nysits.01live/livedev/docroot',
+);
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site nysits, environment 01test
+$aliases['01test'] = array(
+  'root' => '/var/www/html/nysits.01test/docroot',
+  'ac-site' => 'nysits',
+  'ac-env' => '01test',
+  'ac-realm' => 'enterprise-g1',
+  'uri' => 'nysits01test.enterprise-g1.acquia-sites.com',
+  'site' => 'nysits',
+  'env' => '01test',
+  'remote-host' => 'staging-538.enterprise-g1.hosting.acquia.com',
+  'remote-user' => 'nysits.01test',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['01test.livedev'] = array(
+  'parent' => '@nysits.01test',
+  'root' => '/mnt/gfs/nysits.01test/livedev/docroot',
+);
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site nysits, environment 01testup
+$aliases['01testup'] = array(
+  'root' => '/var/www/html/nysits.01testup/docroot',
+  'ac-site' => 'nysits',
+  'ac-env' => '01testup',
+  'ac-realm' => 'enterprise-g1',
+  'uri' => 'nysits01testup.enterprise-g1.acquia-sites.com',
+  'site' => 'nysits',
+  'env' => '01testup',
+  'remote-host' => 'staging-538.enterprise-g1.hosting.acquia.com',
+  'remote-user' => 'nysits.01testup',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['01testup.livedev'] = array(
+  'parent' => '@nysits.01testup',
+  'root' => '/mnt/gfs/nysits.01testup/livedev/docroot',
+);
+if (!isset($drush_major_version)) {
+  $drush_version_components = explode('.', DRUSH_VERSION);
+  $drush_major_version = $drush_version_components[0];
+}
+// Site nysits, environment 01update
+$aliases['01update'] = array(
+  'root' => '/var/www/html/nysits.01update/docroot',
+  'ac-site' => 'nysits',
+  'ac-env' => '01update',
+  'ac-realm' => 'enterprise-g1',
+  'uri' => 'nysits01update.enterprise-g1.acquia-sites.com',
+  'site' => 'nysits',
+  'env' => '01update',
+  'path-aliases' => array(
+    '%drush-script' => 'drush' . $drush_major_version,
+  )
+);
+$aliases['01update.livedev'] = array(
+  'parent' => '@nysits.01update',
+  'root' => '/mnt/gfs/nysits.01update/livedev/docroot',
+);
