9b74bdbb03711ef63a84bacdfc0f167b4633c56b
Lindsey Catlett <lindsey.catlett@acquia.com>
2016-09-08 09:02:22 -0400

    NDD-602: Removed require once from local drush config.

--- drush/drushrc.php
+++ drush/drushrc.php
@@ -60,7 +60,11 @@
  */
 
 if (file_exists(__DIR__ . '/../docroot/sites/default/local.drushrc.php')) {
-  require_once __DIR__ . '/../docroot/sites/default/local.drushrc.php';
+  require __DIR__ . '/../docroot/sites/default/local.drushrc.php';
+}
+
+if (getenv('TUGBOAT_URL')) {
+  $options['uri'] = $_ENV['TUGBOAT_URL'];
 }
 
 // Specify the base_url that should be used when generating links
