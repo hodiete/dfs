9662debaada09646851d998d111421b2013567fe
Lindsey Catlett <lindsey.catlett@acquia.com>
2016-09-08 10:54:41 -0400

    NDD-123: This is a good commit.

--- docroot/sites/default/settings/cache.settings.php
+++ docroot/sites/default/settings/cache.settings.php
@@ -12,3 +12,18 @@ if ($is_ah_env) {
       break;
   }
 }
+
+
+# Memcache configuration.		
+ if (isset($_ENV['AH_SITE_ENVIRONMENT']) && $_ENV['AH_SITE_ENVIRONMENT'] != '01live' && isset($conf['memcache_servers'])) {		
+     # E.g. use memcache for cache_discovery		
+     $settings['cache']['bins']['discovery'] = 'cache.backend.memcache';		
+     # Set up a default cache bin for memcache		
+     $settings['memcache']['bins'] = [		
+         'default' => 'default'		
+     ];		
+     # Acquia is in the process of upgrading the platform-provided memcache		
+     # settings. This is the recommended approach for now.		
+     $settings['memcache']['servers'] = $conf['memcache_servers'];		
+     $settings['memcache']['key_prefix'] = $conf['memcache_key_prefix'];		
+ }
