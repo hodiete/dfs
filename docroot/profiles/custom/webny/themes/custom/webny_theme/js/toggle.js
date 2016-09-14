4fbfa246d683f7500494e6cf4c484a51cc8f357f
Meredith Case <meredith.case@its.ny.gov>
2016-09-14 09:56:54 -0400

    ndd-535: toggle disappears on desktop with media queries, toggle goes from expand to collapse, styling.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/toggle.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/toggle.js
@@ -9,54 +9,9 @@
 
   Drupal.behaviors.filterToggle = {
     attach: function (context, settings) {
-        //console.log('Firing');
-    	/*var flip = 0;
-        $( "button" ).click(function() {
-
-         	//$( ".test" ).toggle( flip++ % 2 === 0 );
-         	if($( ".ptt" ).html() == 'HELLO'){
-         		console.log('Firing');
-         		$(".ptt").hide();
-         	} else {
-         		$(".ptt").show();
-         	}
-
-           
-        });*/
-
 
       var clickVals = 'click touchend';
-      // NUMERIC VALUES
-var maxDesktop          = 1024; // WEBNY STANDARD
-// var maxMob              = 768; // IF STYLES NEEDED UNDER 768 for mobile
-var startBrowserWidth  = window.innerWidth;
-      var curViewMode         = getViewMode(maxDesktop,startBrowserWidth);
-
-/*
-        $('.filterTog').click(function(){
-        	
-        	if($('.filterBody').hasClass('filterBodyDisplay')){
-        		$('.filterBody').removeClass('filterBodyDisplay');
-        		$('.filterBody').addClass('filterBodyHidden');
-        	} else {
-        		$('.filterBody').removeClass('filterBodyHidden');
-        		$('.filterBody').addClass('filterBodyDisplay');
-        	}
-
-    	});
-   */ 	
-      $(document).ready(function(){
-
-        // NORMAL NAV LOADER
-    if(curViewMode == 'Desktop'){
-        $('.filterTog').addClass('filterTogHidden');
-        $('.filterTog').removeClass('filterTogDisplay');
-    } else { // MOBILE NAV LOADER
-        $('.filterTog').removeClass('filterTogHidden');
-          $('.filterTog').addClass('filterTogDisplay');
-    } // END ELSE
-
-});
+      
 
     	$('.filterTog').on(clickVals,function (){
 
@@ -65,9 +20,11 @@ var startBrowserWidth  = window.innerWidth;
           if($('.filterBody').hasClass('filterBodyDisplay')){
         	$('.filterBody').removeClass('filterBodyDisplay');
         	$('.filterBody').addClass('filterBodyHidden');
+          $('.filterTogDisplay').attr('value','Expand');
           } else {
         	$('.filterBody').removeClass('filterBodyHidden');
         	$('.filterBody').addClass('filterBodyDisplay');
+          $('.filterTogDisplay').attr('value','Collapse');
           }
 
     	});
