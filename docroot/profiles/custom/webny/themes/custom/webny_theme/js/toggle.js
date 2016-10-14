4b2d37709cb7c965802a4ec198c25e5480e8ce73
John Pardavila <john.pardavila@its.ny.gov>
2016-10-14 15:08:37 -0400

    NDD-700: Added resize functionality for the toggle.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/toggle.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/toggle.js
@@ -10,33 +10,89 @@
     Drupal.behaviors.filterToggle = {
         attach: function (context, settings) {
 
-            var clickVals = 'click touchend';
+            var clickVals           = 'click touchend';
+            var changeNavEventMode  = false;
+            var curViewMode         = getDisplay(window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
 
-            // HANDLES EVENTS FOR TOGGLE BUTTON AND HEADER REGION
-            $('.results-exposed-filters-title, .filterTogDisplay').on(clickVals,function (e){
+            console.log('This was fired');
+            function resizeListener() {
+                $(window).resize(function (e) {
+                    var maxMobile   = 1024;
+                    var curWidth    = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-                // PREVENT MULTI EVENT FUN
+                    // DETERMINE IF MOBILE AND SHOULD BE DESKTOP
+                    if (curWidth >= maxMobile && curViewMode == 'mobile' && changeNavEventMode === false) {
+                        curViewMode = 'desktop';
+                        changeNavEventMode = true;
+                        console.log(curWidth + ' ' + curViewMode + ' ' + changeNavEventMode + ' ' + maxMobile);
+                    }
+
+                    // DETERMINE IF DESKTOP AND SHOULD BE MOBILE
+                    if (curWidth < maxMobile && curViewMode == 'desktop' && changeNavEventMode === false ) {
+                        curViewMode = 'mobile';
+                        changeNavEventMode = true;
+                        console.log(curWidth + ' ' + curViewMode + ' ' + changeNavEventMode + ' ' + maxMobile);
+                    }
+
+                    // ENABLE OR DISABLE THE EVENT TO TOGGLE
+                    if (changeNavEventMode === true ) {
+
+                        if(curViewMode == 'mobile') {
+                            toggleNewsOpts();
+                        } else {
+                            $(document).off('click touchend','.results-exposed-filters-title, .filterTogDisplay' );
+                        }
+                    }
+
+                    changeNavEventMode = false;
+                });
+            }
+
+            function toggleNewsOpts() {
+                // HANDLES EVENTS FOR TOGGLE BUTTON AND HEADER REGION
+                $('.results-exposed-filters-title, .filterTogDisplay').on(clickVals, function (e) {
+
+                    // PREVENT MULTI EVENT FUN
                     e.stopPropagation();
                     e.preventDefault();
 
-                    console.log(e.type);
+                    // ADD CLASSES TO SHOW AND HIDE FILTER
+                    if ($('.filterBody').hasClass('filterBodyDisplay')) {
 
-                // ADD CLASSES TO SHOW AND HIDE FILTER
-                    if($('.filterBody').hasClass('filterBodyDisplay')){
-
-                            $('.filterBody').removeClass('filterBodyDisplay');
-                            $('.filterBody').addClass('filterBodyHidden');
-                            $('.filterTogDisplay').attr('value','Expand');
+                        $('.filterBody').removeClass('filterBodyDisplay');
+                        $('.filterBody').addClass('filterBodyHidden');
+                        $('.filterTogDisplay').attr('value', 'Expand');
 
                     } else {
 
                         $('.filterBody').removeClass('filterBodyHidden');
                         $('.filterBody').addClass('filterBodyDisplay');
-                        $('.filterTogDisplay').attr('value','Collapse');
-
+                        $('.filterTogDisplay').attr('value', 'Collapse');
                     }
 
-            });
+                });
+
+            }
+
+            function getDisplay(browserWidth){
+                if(browserWidth < 1024){
+                    var display = 'mobile';
+                } else {
+                    var display = 'desktop';
+                }
+                return display;
+            }
+
+            // ######################################################################################
+            /// INIT
+                if (curViewMode == 'mobile') {
+                    toggleNewsOpts();
+                }
+
+            // LISTEN TO RESIZE
+                resizeListener();
+
+
 
         }
     };
