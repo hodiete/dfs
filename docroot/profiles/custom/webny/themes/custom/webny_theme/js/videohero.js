a9681833e971ec7388ce1433874e07d9e11ce01c
John Pardavila <iomaxstudios@hotmail.com>
2017-04-01 17:51:46 -0400

    NDD-923: Updated the javascript to include cosmetic updates and functionality for hero.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/videohero.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/videohero.js
@@ -16,20 +16,41 @@
             // HERO BUTTON  .video_hero_button
 
             var clickVals = 'click touchend';
+            var bkg_image = null;
+            var bkg_color = $('.hero-no-image').css('background-image');;
 
             // VIDEO BUTTON EVENT
-            $('.video_hero_button > a, .hero-hide-video > a', context).on(clickVals, function (e) {
+            $('.video_hero_button > a, .hero-video-close > a', context).on(clickVals, function (e) {
 
                 // PREVENT MULTI EVENT FUN
                 e.stopPropagation();
                 e.preventDefault();
 
+                // SHOW VIDEO
                 if ($('.hero-video-frame').hasClass('hero-video-hide')) {
+
+                    // START VIDEO
+
+
+                    // CONTROL ENVIRONMENT
                     $('.hero-video-frame').removeClass('hero-video-hide').addClass('hero-video-show');
                     $('.hero-inner').hide();
-                } else {
+                    $('.hero-has-image').addClass('hero-bkg-removed');
+                    $('.hero-bkg').css('height', '0');
+                    $('.hero-no-image').addClass('hero-meta-change');
+
+                } // HIDE VIDEO
+                else {
+
+                    // STOP VIDEO
+
+
+                    // CONTROL ENVIRONMENT
                     $('.hero-video-frame').removeClass('hero-video-show').addClass('hero-video-hide');
                     $('.hero-inner').show();
+                    $('.hero-has-image').removeClass('hero-bkg-removed');
+                    $('.hero-bkg').css('height', '100%');
+                    $('.hero-no-image').removeClass('hero-meta-change');
                 }
 
             });
