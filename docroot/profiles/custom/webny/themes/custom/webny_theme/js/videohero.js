10014afc64841ead57cefd83298efe56538a14f3
John Pardavila <iomaxstudios@hotmail.com>
2017-04-12 12:49:15 -0400

    NDD-923: Made various changes to the javascript file to handle click/touch processing.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/videohero.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/videohero.js
@@ -12,81 +12,71 @@ var videojs = typeof videojs == 'undefined' ? {} : videojs;
   Drupal.behaviors.videohero = {
     attach: function (context, settings) {
 
-      // HERO HEADER  .hero-header
-      // HERO INNER   .hero-inner
-      // HERO VIDEO   .hero-video-frame
-      // HERO BUTTON  .video_hero_button
-
       var clickVals = 'click touchend';
-      var id = $('.hero-video-player').attr('id');
-      var heroPlayer = videojs($('.hero-video-player').attr('id'));
+      var brightPlayerHTML = null;
+      var heroPlayer = null;
 
-      function initHeroVideo(src, tech, id){
+      function initHeroVideo(src, tech, id) {
 
         // VIDEO BUTTON EVENT
-        $('.video_hero_button > a, .hero-video-close > a', context).on(clickVals, function (e) {
+        $('.video_hero_button > a', context).on(clickVals, function (e) {
 
           // PREVENT MULTI EVENT FUN
           e.stopPropagation();
           e.preventDefault();
 
-          // GET THE SOURCE TO CHECK IF BRIGHTCOVE/VIMEO/YOUTUBE
-
           // SHOW VIDEO
           if ($('.hero-video-frame').hasClass('hero-video-hide')) {
 
-
             // DETERMINE IF BRIGHTCOVE AND INJECT INTO VIDEO TAG
             if (src.indexOf('brightcove') !== -1) {
-              heroPlayer = '<iframe src=\"//players.brightcove.net/' +
+              brightPlayerHTML = '<iframe src=\"//players.brightcove.net/' +
                 src.replace(/^.*\/\/[^\/]+/, '') + '\"' + ' allowfullscreen webkitallowfullscreen mozallowfullscreen ' +
-                'class="video-js hero-video-player"></iframe>';
-              window.document.getElementById(id).outerHTML = heroPlayer;
+                'class="video-js hero-video-player" id="' + id + '"></iframe>';
+              window.document.getElementById(id).outerHTML = brightPlayerHTML;
             }
 
             // CONTROL ENVIRONMENT
-            $('.video-js').removeClass('vjs-vimeo');
             $('.hero-video-frame').removeClass('hero-video-hide').addClass('hero-video-show');
             $('.hero-inner').hide();
-            $('.hero-has-image').addClass('hero-bkg-removed');
-            $('.hero-bkg').css('height', '0');
-            $('.hero-no-image').addClass('hero-meta-change');
-
-            // VIMEO OVERRIDES
-            if(tech === 'Vimeo') {
-              $('.vimeoFrame').css('padding-bottom', '56%')
-                .css('width','758px');
-            }
-
+            $('.hero-header').hide();
 
             // START VIDEO
-            heroPlayer.play();
-
-          } // HIDE VIDEO
-
-          else {
-
-            // CONTROL ENVIRONMENT
-            $('.hero-video-frame').removeClass('hero-video-show').addClass('hero-video-hide');
-            $('.hero-inner').show();
-            $('.hero-has-image').removeClass('hero-bkg-removed');
-            $('.hero-bkg').css('height', '100%');
-            $('.hero-no-image').removeClass('hero-meta-change');
-
-            // PAUSE VIDEO
-            heroPlayer.pause();
+            if(tech === 'youtube' || tech === 'Vimeo') {
+              heroPlayer = videojs(id);
+              heroPlayer.play();
+            }
           }
 
         });
 
-      }
+        // ============================================================================================
+        // VIDEO BUTTON EVENT -- CLOSE FRAME AND PAUSE VIDEO
+        $('.hero-video-close > a', context).on(clickVals, function (e) {
+
+          // CONTROL ENVIRONMENT
+          $('.hero-video-frame').removeClass('hero-video-show').addClass('hero-video-hide');
+          $('.hero-inner').show();
+          $('.hero-header').show();
+
+          // PAUSE VIDEO
+          if(tech === 'youtube' || tech === 'Vimeo') {
+            heroPlayer.pause();
+          }
+        });
+
+      } // END FUN
+
 
       // START INIT OF THE HERO VIDEO
       $('.hero-video-player').each(function (index, el) {
+
         var $ds = jQuery.parseJSON(el.getAttribute('data-setup'));
         var src = $ds.sources['0'].src;
         var tech = $ds.techOrder['0'];
+
         initHeroVideo(src, tech, el.id);
+
       });
 
     }
