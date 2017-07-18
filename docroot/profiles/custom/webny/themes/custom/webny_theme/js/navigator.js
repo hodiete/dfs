afe920564a8540548dac7ff2fbbf373a963ab51c
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2017-07-18 14:36:08 -0400

    NDD-1002: fix jslint errors in JS.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/navigator.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/navigator.js
@@ -9,29 +9,29 @@
 
   Drupal.behaviors.navigator = {
     attach: function (context, settings) {
-    //init:function() {
       // Loop through each section (paragraph)
-      $('.gp-chapters section').once().each(function() {
-        // define the hash being used for anchoring (grab title from paragraph, transforming to lowercase,
-        // replacing spaces with dash)
+      $('.gp-chapters section').once().each(function () {
+        // define the hash being used for anchoring grab title from paragraph, transforming to lowercase,
+        // replacing spaces with dash
         var hash = $(this).children(':first').text().toLowerCase().replace(/ /g, "-");
-        if ($(this).attr('rel') == 'enabled') {
+        if ($(this).attr('rel') === 'enabled') {
           $('#gpnav_sidebar ul').append('<li><a href="#' + hash + '">' + $(this).children(':first').text() + '</a></li>');
           $(this).children(':first').attr('name', hash);
         }
       });
 
-      $('#gpnav_sidebar ul li.see-all').once().click(function(e) {
+      $('#gpnav_sidebar ul li.see-all').once().click(function (e) {
         e.preventDefault();
 
         if ($(this).parent().hasClass('sidebar-closed')) {
           $(this).parent().removeClass('sidebar-closed').addClass('sidebar-opened');
-        } else if ($(this).parent().hasClass('sidebar-opened')) {
+        }
+        else if ($(this).parent().hasClass('sidebar-opened')) {
           $(this).parent().removeClass('sidebar-opened').addClass('sidebar-closed');
         }
       });
 
-      $('#gpnav_sidebar ul li a').click(function(e) {
+      $('#gpnav_sidebar ul li a').click(function (e) {
         e.preventDefault();
         if (!$(this).parent().hasClass('see-all')) {
           // name used on title of paragraph
@@ -61,15 +61,15 @@
       });
 
       // on load check to see if there is a hash anchor in the URL to force an auto scroll to section on page
-      $(window).on('load', function() {
+      $(window).on('load', function () {
         // grab hash from URL (#variable)
         var hash = window.location.hash;
         // if hash exists
-        if(hash){
-          //NY.OneStopArticle.deep_linking = true;
+        if(hash) {
           // trigger a click on the sidebar a where href == hash essentialy loading the page and faking a click moving the scroll down
-          $('#gpnav_sidebar li a[href="'+hash+'"]').trigger('click');
-        } else {
+          $('#gpnav_sidebar li a[href="' + hash + '"]').trigger('click');
+        }
+        else {
           // set first item in toc as active on load if there is no hash
           $('#gpnav_sidebar ul').children(':nth-child(2)').addClass('active');
         }
@@ -79,53 +79,56 @@
           // add class to mark ul as closed
           $('#gpnav_sidebar ul').addClass('sidebar-closed');
           $('#gpnav_sidebar ul').addClass('mobile');
-        } else {
+        }
+        else {
           $('#gpnav_sidebar ul').addClass('sidebar-opened');
         }
 
       });
 
-      $(window).on('resize', function() {
+      $(window).on('resize', function () {
         // if see-all is visible meaning in mobile view
         if ($('#gpnav_sidebar ul li.see-all').is(':visible')) {
           // add class to mark ul as closed
           $('#gpnav_sidebar ul').removeClass('sidebar-opened');
           $('#gpnav_sidebar ul').addClass('sidebar-closed');
           $('#gpnav_sidebar ul').addClass('mobile');
-        } else {
+        }
+        else {
           $('#gpnav_sidebar ul').removeClass('sidebar-closed');
           $('#gpnav_sidebar ul').addClass('sidebar-opened');
           $('#gpnav_sidebar ul').removeClass('mobile');
         }
       });
 
-      $('.actions').waypoint(function(direction) {
-        //console.log('Scrolled to waypoint!');
+      $('.actions').waypoint(function (direction) {
         if (direction === 'down') {
           $('.actions').addClass('stuck');
           $('#gpnav_sidebar').addClass('stuck');
-        } else {
+        }
+        else {
           $('.actions').removeClass('stuck');
           $('#gpnav_sidebar').removeClass('stuck');
         }
       });
 
-      $('.gp-chapters section').waypoint(function(direction) {
+      $('.gp-chapters section').waypoint(function (direction) {
         var sectonId = this.element.id;
         var sectionName = $('#' + sectonId).children(':first').attr('name');
-        $('#gpnav_sidebar ul li').each(function() {
-          if ($(this).children('a').attr('href') == '#' + sectionName) {
+        $('#gpnav_sidebar ul li').each(function () {
+          if ($(this).children('a').attr('href') === '#' + sectionName) {
             $(this).addClass('active');
             $(this).show();
             var item = $('#gpnav_sidebar ul li.active');
             if (item.position()) {
-              $('#gpnav_sidebar span.arrow').css({top: Math.round(item.position().top + item.height()/2 + 2) + 'px'});
+              $('#gpnav_sidebar span.arrow').css({top: Math.round(item.position().top + item.height() / 2 + 2) + 'px'});
             }
-          } else {
+          }
+          else {
             $(this).removeClass('active');
           }
         });
-      }, { offset: 100 });
+      }, {offset: 100});
     }
   };
 
