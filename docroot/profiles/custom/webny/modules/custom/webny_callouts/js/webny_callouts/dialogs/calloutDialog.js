08eba7591a7d1cb131aaa949a0a39722f45e3feb
John Pardavila <iomaxstudios@hotmail.com>
2017-08-10 20:20:55 -0400

    NDD-1003: Added callout module.

---
+++ docroot/profiles/custom/webny/modules/custom/webny_callouts/js/webny_callouts/dialogs/calloutDialog.js
@@ -0,0 +1,182 @@
+﻿/**
+ * Basic sample plugin inserting callouts into CKEditor editing area.
+ */
+
+// Register the plugin within the editor.
+(function($) {
+    "use strict";
+
+    // ========================================================================================================
+    CKEDITOR.dialog.add( 'calloutDialog', function( editor ) {
+
+        return {
+            editorName: false,
+            title: 'Add/Edit Callouts',
+            minWidth: 500,
+            minHeight: 300,
+            callouts_el: false,
+
+            contents: [
+                {
+                    id: 'tab-callout',
+                    label: 'Callout Settings',
+                    elements: [
+                        {
+                            // THE BODY OF THE CALLOUT
+                            type: 'textarea',
+                            id: 'callout_body',
+                            className: 'callout_body',
+                            label: 'Callout: Body Field',
+                            inputStyle: 'height: 125px',
+                            validate: CKEDITOR.dialog.validate.notEmpty( "Please enter a value for the body of the Callout field." )
+                        },
+                        {
+                            // TEXT FROM THE WYSIWYG
+                            type: 'text',
+                            id: 'callout_text',
+                            name: 'callout_text',
+                            label: 'Text to Highlight in Body',
+                            validate: CKEDITOR.dialog.validate.notEmpty( "Please enter text that will be highlighted in the body of the page." ),
+                        },
+                        {
+                            type: 'html',
+                            html: '<br /><div class="callout-delete-desc-html"><label>Delete Callout?</label><p>Click the \'Delete Callout\' button above to remove this callout from this editor. </p></div>'
+                        },
+                        {
+                            // DELETE BUTTON
+                            type: 'button',
+                            id: 'delete_callout_entry',
+                            label: 'Delete Callout',
+                            title: 'Delete this callout.',
+                            onClick: function() {
+
+                                // CALLOUTS OBJ CLASS
+                                var coObj = CKEDITOR.plugins.callouts;
+
+                                // INSERT DELETED TEXT
+                                $(coObj.editorDom.body).empty().append(coObj.editorBodyDeleteHTML);
+
+                                // REMOVE CALLOUT AND CURRENT DIALOG -- HAPPENS BY DEFAULT ON EDIT
+                                CKEDITOR.dialog.getCurrent().hide();
+                            }
+                        },
+                        {
+                            type: 'html',
+                            html: '<br />'
+                        }
+                    ]
+                }
+            ],
+
+            onShow: function() {
+
+                // CALLOUTS OBJ CLASS
+                var coObj = CKEDITOR.plugins.callouts;
+
+                // EDITOR DOCUMENT AND $
+                coObj.editorDom = editor.document.$;
+
+                // GET THE WHOLE TEXT -- JUST IN CASE -- NOT THE MOST EFFICIENT
+                coObj.editorBodyHTML         = $(editor.document.$.body).html();
+                coObj.editorBodyDeleteHTML   = $(editor.document.$.body).html();
+
+                // FIND IF TEXT IS WITHIN A CALLOUT -- BRING BACK VALUES
+                var selectedTag             = coObj.getInlineCalloutTag(editor);
+                coObj.selectedHTML          = coObj.getInlineCalloutHTML(editor);
+                var classNames              = coObj.getSelInlineCalloutClassNames(editor);
+
+                // =============== EDIT CALLOUT ================
+                if(selectedTag === 'SPAN' && classNames.indexOf('web-callout') !== -1) {
+
+                    // GET ID OF SPECIFIC CALLOUT BODY VIA DATA ID
+                    var icoid = coObj.getInlineCalloutIdNum(editor);
+
+                    // GET CALLOUT BODY HTML -- BACKUPS
+                    coObj.spanClause = $(coObj.editorDom.getElementById('callout-order-' + icoid));   // GET SPAN;
+                    coObj.bodyClause = $(coObj.editorDom.getElementById('bco-' + icoid));             // GET BODY;
+                    $(coObj.editorDom.getElementById('callout-order-' + icoid)).remove();             // REMOVE THE SPAN
+                    var calloutBody = $(coObj.editorDom.getElementById('bco-' + icoid)).children('div').html(); // DIV
+
+                    // REMOVE THE ENTIRE BODY BEFORE REBUILDING
+                    $(coObj.editorDom.getElementById('bco-' + icoid)).remove();
+
+                    // REMOVE THE INLINE CALLOUT
+                    $(coObj.editorDom.getElementById('ico-'+ icoid)).remove();
+
+                    // SET BODY TO DIALOG
+                    this.setValueOf('tab-callout', 'callout_body', calloutBody);
+
+                    // SET THE VALUE OF THE POINT WHERE SELECTED
+                    this.setValueOf('tab-callout', 'callout_text', coObj.selectedHTML);
+
+                    // THE KICKER -- DELETE ENTIRE CONTENTS IF NO ENTRIES
+                    // REBUILD ADDS NEW INLINE AND BODY FIELDS
+                    var innerBodyCalloutHTML    = coObj.getBcoInnerChildrenHTML(coObj.editorDom, coObj.hash);
+                    if(innerBodyCalloutHTML === ''){
+                        $(coObj.editorDom.getElementById('webny-callouts-section-' + coObj.hash)).remove();
+                    }
+
+                } else {
+                    // =============== NEW CALLOUT ================
+                    // SET THE VALUE OF THE TEXT HIGHLIGHTED
+                    this.setValueOf('tab-callout', 'callout_text', editor.getSelection().getSelectedText().toString());
+                }
+
+                // =============== PREPARE DELETE STRING OBJ ===============
+
+                // ASSIGN THE NEW DELETE STRING TO THE DELETE STRING PROPERTY
+                coObj.editorBodyDeleteHTML = coObj.prepareDeleteString(editor,icoid);
+
+                // HIDE DELETE BUTTON AND TEXT IF EMPTY
+                var dialogDelete = $('.callout-delete-desc-html, .cke_dialog_ui_vbox .cke_dialog_ui_button');
+                dialogDelete.hide();
+
+                if(calloutBody !== '' && calloutBody !== undefined){
+                    dialogDelete.show();
+                }
+
+            },
+            onOk: function() {
+                    var dialog = this;
+
+                    // ADD LOGIC TO GET TEXT FROM FIELDS AND WRAP IN TAGS
+
+                    // GET THE PLUGIN CALLOUT OBJECT
+                    var coObj = CKEDITOR.plugins.callouts;
+
+                    // GET RANDOM ID
+                    var randcoid = coObj.getRandomCalloutId() ;
+
+                    // GET EDITOR
+                    var thisEditor = coObj.getCurrentEditor();
+
+                    // GET VALUES FROM FIELDS
+                    var co_text = dialog.getContentElement('tab-callout','callout_text').getValue();
+                    var co_body = dialog.getContentElement('tab-callout','callout_body').getValue();
+
+                    // CREATE THE INLINE CALLOUT
+                    var inline_value = '';
+
+                    inline_value = CKEDITOR.dom.element.createFromHtml('<span data-ico="'+randcoid+'" id=\x22ico-'+randcoid+'\x22 ' +
+                        'class=\x22web-callout inline-callout callout-'+ randcoid+'\x22>' +
+                        co_text + '</span>');
+
+                    CKEDITOR.instances[thisEditor].insertElement(inline_value);
+
+                    // BUILD THE CALLOUT -- RETURN calloutSettings
+                    var calloutSettings = coObj.buildCallouts(CKEDITOR.instances[thisEditor], randcoid, co_body);
+
+                },
+                onCancel: function() {
+
+                    // CALLOUTS OBJ CLASS
+                    var coObj = CKEDITOR.plugins.callouts;
+
+                    // EMPTY BODY AND APPEND ORIGINAL BODY IN THE EDITOR
+                    $(coObj.editorDom.body).empty().append(coObj.editorBodyHTML);
+
+            }
+        };
+    });
+
+}(window.jQuery));
---
+++ docroot/profiles/custom/webny/modules/custom/webny_callouts/js/webny_callouts/plugin.js
@@ -0,0 +1,386 @@
+﻿/**
+ * Basic sample plugin inserting callouts into CKEditor editing area.
+ */
+
+// Register the plugin within the editor.
+(function($) {
+    "use strict";
+
+    CKEDITOR.plugins.add('webny_callouts', {
+
+        init: function(editor) {
+
+            // Add some CSS tweaks:
+            var css = 'span.inline-callout {color: #fff; background-color: #f55; margin-left: 2px; margin-right: 2px;' +
+                'padding: 3px 5px; border-left: 2px solid #000; border-right: 2px solid #000} ' +
+                'div.inline-callout-section { display: none }' +
+                '#delete_callout_entry{ background-color: #c22; color: #fff; font-style:bold;}';
+            CKEDITOR.addCss(css);
+
+            // ========================================================================================================
+            // ADDS THE DIALOG COMMAND -- THE DIALOG TO THE CALLOUT LOGIC
+            editor.addCommand( 'calloutDialog', new CKEDITOR.dialogCommand('calloutDialog'), {
+
+            });
+
+            CKEDITOR.dialog.add('calloutDialog', this.path + 'dialogs/calloutDialog.js' );
+
+            // ========================================================================================================
+            // THIS ADDS THE BUTTON TO THE EDITOR
+            editor.ui.addButton('WebNYCallouts', {
+                label: Drupal.t('Create or Edit a Callout'),
+                command: 'calloutDialog',
+                icon: this.path + 'icons/webny_callouts_sm.png',
+            });
+
+            // ========================================================================================================
+            // CHECK FOR THE CKEDITOR BEING EDITED
+            editor.on('instanceReady', function (e) {
+                var currentEditorName = null;
+                for (var id in CKEDITOR.instances) {
+                    CKEDITOR.instances[id].on('focus', function(e) {
+
+                        // SET CURRENT EDITOR NAME
+                        CKEDITOR.plugins.callouts.setCurrentEditor(e.editor.name);
+                    });
+                }
+            });
+        }
+
+    }); // END PLUGIN ADD
+
+}(window.jQuery));
+
+// CREATE SUB OBJECT METHODS AND PROPERTIES AS HELPERS
+CKEDITOR.plugins.callouts = {
+
+    // JS PROPS
+    cen: null,
+    cida: [],
+    calloutSectionTag: null,
+    salt: null,
+    saltBag: [],
+    hash: null,
+    hashLength: 7,
+    spanClause: null,
+    bodyClause: null,
+    editorHTMLObj: null,
+    editorBodyHTML: null,
+    editorBodyDeleteHTML: null,
+    selectedHTML: null,
+    editorDom: null,
+
+    // JS METHODS
+    /*
+        SETS THE CURRENT EDITOR IN USE
+     */
+    setCurrentEditor: function(cen){
+        this.cen = cen;
+    },
+    /*
+     GETS THE CURRENT EDITOR
+     */
+    getCurrentEditor: function() {
+        return this.cen;
+    },
+
+    setSaltBag: function(){},
+
+    getSaltBag: function(){
+        return this.saltBag;
+    },
+
+    /*
+        GENERATES A RANDOM ID
+     */
+    getRandomCalloutId: function() {
+
+        // GET RANDOM UP TO 100 ID, ENSURE NOT TAKEN IN CIDSECTION ARRAY -- via cida
+        var rnum = Math.floor(Math.random() * 9999) + 1;
+
+        if(this.cida.indexOf(rnum) === -1 ){
+            this.cida.push(rnum);
+            return rnum;
+
+        } else {
+            // RECALL
+            this.getRandomCalloutId();
+        }
+    },
+    /*
+        SET THE CALLOUT BODY TAG FOR NEW ENTRIES
+        Return: String
+     */
+    setCalloutBodySectionTag: function(hash){
+        return '<div data-ckhash="'+hash+'" id="webny-callouts-section-'+hash+'" ' +
+            'class="webny-callouts-section inline-callout-section"><div class="webny-callout-inner">';
+    },
+    /*
+       GET THE CALLOUT BODY TAG
+       Return: String hash
+    */
+    getCalloutBodySectionTag: function(editor){
+        return editor.document.$.getElementsByClassName('webny-callouts-section');
+    },
+    getSectionTagId: function(editor){
+        return $(editor.document.$.getElementsByClassName('webny-callouts-section')).attr('id');
+    },
+
+    createCalloutHash: function(editor){
+
+        // GET THE EDITOR ID
+        var editorID    = editor.id;
+
+        // GET THE NUMERIC VALUE OF THE EDITOR ID
+        var editorIDNum = editorID.substring(4,10);
+
+        // VAR FOR NEW HASH
+        var newHash     = null;
+
+        // THERE IS A SALT IN THE DOM FOR THIS EDITOR -- USE THIS SALT
+        if(this.getCalloutBodySectionTag(editor).length > 0){
+
+            // GET CURRENT HASH, SET IT TO THE CALLOUT STRING, PUSH TO SALTBAG
+            newHash = this.getSectionTagId(editor).substring(23,40);
+
+        } else {
+
+            // CREATE A NEW CALLOUT TAG WITH A NEW HASH
+            newHash = this.checkSaltBag(null, this.saltBag) + '-' + editorIDNum;
+        }
+
+        return newHash;
+
+    },
+    buildCallouts: function(editor, newCalloutID, newBody) {
+
+        // REORDER IF POSSIBLE
+        var fixedCalloutBody = this.rebuildCalloutBody(editor);
+
+        if(fixedCalloutBody !== null){
+
+            $(editor.document.$.getElementsByClassName('webny-callouts-section')).remove();
+            $(editor.document.$.body).append(fixedCalloutBody);
+            console.log(this.editorDom.body);
+        }
+
+        // INIT VARS
+        var calloutBody, calloutMarker, calloutText, calloutID, newCOIDString, calloutHash, curNumCalloutID = null;
+
+        // GET TOTAL EDITOR AND CALLOUTS
+        var calloutsInlineArray     = editor.document.$.getElementsByClassName('inline-callout');
+
+        // GET THE BODY OBJECT OF THE CURRENT EDITOR
+        this.editorHTMLObj          = editor.document.$.body;
+        this.hash                   = this.createCalloutHash(editor);
+
+        // SET THE SECTION TAG STRING AND ASSIGN IT TO THE CALLOUT SECTION HTML
+        this.calloutSectionTag = this.setCalloutBodySectionTag(this.hash);
+        var calloutSectionHTML = this.calloutSectionTag;
+
+        // REBUILD CALLOUT BODY SECTION WITH CALLOUT NUMBERS AND BODY
+        if(calloutsInlineArray.length > 0){
+            for (var x = 0; x < calloutsInlineArray.length; x++) {
+
+                calloutMarker   = x + 1;
+                calloutText     = calloutsInlineArray[x].innerHTML;
+                calloutID       = calloutsInlineArray[x].id;
+                calloutHash     = this.hash;
+                newCOIDString   = this.reassignCalloutID(calloutID);
+                curNumCalloutID = parseInt(calloutID.substring(4,17));
+
+                // GET NEW ENTRY
+                if(curNumCalloutID === newCalloutID){
+
+                    // ASSIGN THE BODY FIELD FROM THE DIALOG
+                    calloutBody = newBody;
+
+                // GET PREEXISTING TEXT
+                } else {
+
+                    // CREATE THE BODY CALLOUT ID AND PASS THE VALUE TO THE CALLOUT BODY PROPERTY
+                    var bodyCalloutID = 'bco-' + curNumCalloutID;
+
+                    // REMOVE THE FIRST DIV AND SPAN - GET RESULTING HTML
+                    $(editor.document.$.getElementById('callout-order-' + curNumCalloutID)).remove(); // REMOVE SPAN
+                    calloutBody = $(editor.document.$.getElementById('bco-' + curNumCalloutID)).children().html(); // DIV
+
+                }
+
+                // CREATE NEW ENTRY
+                calloutSectionHTML += '<div data-ckhash="'+calloutHash+'" data-bco="'+curNumCalloutID+'" ' +
+                    'id="' + newCOIDString + '" class="body-callouts callout-' + newCOIDString + '">' +
+                      '<div class="body-callouts-inner"><span data-order="'+ calloutMarker +'" ' +
+                      'id="callout-order-'+curNumCalloutID+'" ' +
+                      'class="callout-order inline-callout-order callout-order-'+ calloutMarker +'">' + calloutMarker +'</span> ' +
+                    calloutBody.trim() +
+                    '</div></div>';
+
+                calloutBody = null;
+            }
+        }
+
+        // CLOSE THE HTML OF THE CALLOUT SECTION
+        calloutSectionHTML += '</div></div>';
+
+        // REMOVE ANY CALLOUT BODY SECTION - APPEND THE NEW CALLOUT SECTION HTML TO THE BOTTOM OF THE EDITOR BODY
+        if(editor.document.$.getElementById('webny-callouts-section-'+calloutHash) !== null){
+            $(editor.document.$.getElementById('webny-callouts-section-'+calloutHash)).remove();
+        }
+
+        // APPEND THE CALLOUT SECTION TO THE EDITOR
+        $(this.editorHTMLObj).append(calloutSectionHTML);
+
+        // UPDATE THE ELEMENT
+        editor.updateElement();
+
+        // RETURN THE HASH OF VALUES
+        return calloutSettings = {
+            'hash': this.hash
+        }
+    },
+
+    // REBUILD CALLOUT BODY ORDER
+    // TAKES THE CURRENT EDITOR BODY OBJ editor.$.body
+    rebuildCalloutBody: function(editor){
+
+        var calloutBodyHTML = '';
+
+        // CHECK THE EDITOR FOR THE webny-callouts-section CLASS EXISTENCE
+        if(editor.document.$.getElementsByClassName('webny-callouts-section').length > 0){
+
+            // INNER VARIABLES
+            var id = '';
+            var orderNum = null;
+
+            // EASY ACCESSORS
+            var ckhash                  = $(editor.document.$.getElementsByClassName('webny-callouts-section')).attr('data-ckhash');
+            var calloutInnerObj         = $(editor.document.$.getElementsByClassName('webny-callout-inner')).html();
+            var bodyInnerObj            = $(editor.document.$.getElementsByClassName('body-callouts-inner')).html();
+            calloutBodyHTML             = this.setCalloutBodySectionTag(ckhash);
+            var calloutBodyText         = '';
+
+            // TRAVERSE EACH INLINE ENTRY
+            $(calloutInnerObj).each(function (index, value) {
+
+                // LOOP VARS
+                id = $(value)[0].dataset.bco;
+                orderNum = parseInt(index + 1);
+
+                // REMOVE INNER SPAN WITH PREEXISTING ORDER
+                $(value).find('span.callout-order').remove();
+                calloutBodyText = $(value).children().html();
+
+                // CREATE NEW ENTRY
+                calloutBodyHTML += '<div data-ckhash="'+ckhash+'" data-bco="'+id+'" ' +
+                    'id="bco-' + id + '" class="body-callouts callout-' + id + '">' +
+                    '<div class="body-callouts-inner"><span data-order="'+ orderNum +'" ' +
+                    'id="callout-order-'+id+'" ' +
+                    'class="callout-order inline-callout-order callout-order-'+ orderNum +'">' + orderNum +'</span> ' +
+                    calloutBodyText +
+                    '</div></div>';
+
+            });
+
+            calloutBodyHTML += '</div></div>';
+        }
+
+        return calloutBodyHTML;
+
+    },
+
+    // BUILD TEMP EDITOR HTML STRING TO PREP FOR A DELETE
+    prepareDeleteString: function(editor,icoid){
+
+        var ds = '';
+
+        // GO THROUGH EACH DOM OBJ IN THE CKEDITOR BODY
+        $(this.editorBodyDeleteHTML).each(function (index, value) {
+
+            // CHECK EACH ELEMENT
+            if($(value).find('span#ico-'+icoid).length > 0){
+
+                $(value).find('span#ico-'+icoid).contents().unwrap();
+                ds += $(value)[0].outerHTML;
+
+            } else {
+
+                if($(value)[0].classList[0] === 'webny-callouts-section'){
+
+                    // REORDER CALLOUT BODY AREA
+                    ds += CKEDITOR.plugins.callouts.rebuildCalloutBody(editor);
+
+                } else {
+                    ds += $(value)[0].outerHTML;
+                }
+            }
+        });
+
+        return ds;
+    },
+
+
+    /* HELPER CALLOUT METHODS */
+    getInlineCalloutHTML: function(editor) {
+        return editor.getSelection().getStartElement().getHtml();
+    },
+    getInlineCalloutTag: function(editor) {
+        return editor.getSelection().getStartElement().$.tagName;
+    },
+    getSelInlineCalloutClassNames: function(editor){
+        return editor.getSelection().getStartElement().$.className;
+    },
+    getInlineCalloutIdNum: function(editor){
+        return editor.getSelection().getStartElement().$.dataset['ico'];
+    },
+    getBcoInnerChildrenHTML: function(editorDom, hash){
+        return $(editorDom.getElementById('webny-callouts-section-' + hash)).children().html();
+    },
+
+    /*
+        ADDS A BODY SECTION ID TO MATCH WITH INLINE CALLOUT
+    */
+    reassignCalloutID: function(id){
+        return 'bco-'+id.substring(4,17);
+    },
+
+    /*
+        CREATE SALT FOR IDS
+     */
+    calloutSalter: function() {
+
+        // SALT STRING
+        var salt = '';
+
+        // POTENTIAL VALUES FOR SALTER
+        var pvals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+        for(var q=0; q<this.hashLength; ++q) {
+            salt += pvals.charAt(Math.floor(Math.random() * pvals.length));
+        }
+
+        return salt;
+    },
+
+    checkSaltBag: function(salt, bag){
+
+        if(salt === null) {
+            salt = this.calloutSalter();
+        }
+
+        // IF IN LIST
+        if(bag.indexOf(salt) !== -1){
+
+            // CHOSE A NEW SALT
+            salt = this.calloutSalter();
+            this.checkSaltBag(salt,bag);
+
+        } else { // NOT IN LIST
+
+            // PUSH THE NEW SALT TO THE LIST -- RETURN
+            this.saltBag.push(salt);
+            return this.salt = salt;
+        }
+    }
+
+};
