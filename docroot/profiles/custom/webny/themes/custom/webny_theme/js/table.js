449dd09c377fbfeba87febbf7bf192054600e91e
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2017-10-10 14:30:24 -0400

    NDD-1166: fix linting issues with coding styling.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/table.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/table.js
@@ -13,27 +13,34 @@
       $('table').once().dataTable({
         order: [],
         paging: true,
-        "pageLength": 25, 
-        "pagingType": "simple_numbers",
-        "lengthChange": false,
-        "info": false
+        pageLength: 25,
+        pagingType: 'simple_numbers',
+        lengthChange: false,
+        info: false
       });
     }
   };
 })(jQuery, Drupal, this);
 
 (function () {
-	var headertext = [];
-	var headers = document.querySelectorAll("thead");
-	var tablebody = document.querySelectorAll("tbody");
-	
-	for(var i = 0; i < headers.length; i++) { headertext[i]=[]; for (var j = 0, headrow; headrow = headers[i].rows[0].cells[j]; j++) { var current = headrow; headertext[i].push(current.textContent.replace(/\r?\n|\r/,"")); } } if (headers.length > 0) {
-		for (var h = 0, tbody; tbody = tablebody[h]; h++) {
-			for (var i = 0, row; row = tbody.rows[i]; i++) {
-			  for (var j = 0, col; col = row.cells[j]; j++) {
-			    col.setAttribute("data-th", headertext[h][j]);
-			  } 
-			}
-		}
-	}
-} ());
+
+  'use strict';
+  var headertext = [];
+  var headers = document.querySelectorAll('thead');
+  var tablebody = document.querySelectorAll('tbody');
+  for(var t = 0; t < headers.length; t++) {
+    headertext[t] = [];
+    for (var a = 0, headrow; headrow = headers[t].rows[0].cells[a]; a++) {
+      var current = headrow; headertext[t].push(current.textContent.replace(/\r?\n|\r/, ''));
+    }
+  }
+  if (headers.length > 0) {
+    for (var h = 0, tbody; tbody = tablebody[h]; h++) {
+      for (var i = 0, row; row = tbody.rows[i]; i++) {
+        for (var j = 0, col; col = row.cells[j]; j++) {
+          col.setAttribute('data-th', headertext[h][j]);
+        }
+      }
+    }
+  }
+}());
