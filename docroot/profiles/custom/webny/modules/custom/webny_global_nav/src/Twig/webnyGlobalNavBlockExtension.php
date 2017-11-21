4d1184f23b4e6c280329de3a1ef855075d9e96a6
Ryan Grainer <ryan.grainer@its.ny.gov>
2017-11-20 21:20:00 -0500

    NDD-000: reverted 2 files from 1173 work.

--- docroot/profiles/custom/webny/modules/custom/webny_global_nav/src/Twig/webnyGlobalNavBlockExtension.php
+++ docroot/profiles/custom/webny/modules/custom/webny_global_nav/src/Twig/webnyGlobalNavBlockExtension.php
@@ -1,6 +1,5 @@
 <?php
 
-namespace Drupal\Core\Link;
 namespace Drupal\webny_global_nav\Twig;
 
 /**
@@ -31,6 +30,9 @@ class webnyGlobalNavBlockExtension extends \Twig_Extension {
      */
     public function globalNavBlock($menu_name, $type, $format) {
         $menu_tree = \Drupal::menuTree();
+
+        // Build the typical default set of menu tree parameters.
+
         $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name)->onlyEnabledLinks()->setMaxDepth(2);
 
         // Load the tree based on this set of parameters.
@@ -38,79 +40,17 @@ class webnyGlobalNavBlockExtension extends \Twig_Extension {
 
         // Transform the tree using the manipulators you want.
         $manipulators = array(
+            // Only show links that are accessible for the current user.
             array('callable' => 'menu.default_tree_manipulators:checkAccess'),
+            // Use the default sorting of menu links.
             array('callable' => 'menu.default_tree_manipulators:generateIndexAndSort'),
         );
-
-        // GET MENU TREE
         $tree = $menu_tree->transform($tree, $manipulators);
 
-        // ASSIGN HTML CONST
-        $ultop = '<ul class="gnav-ul">';
-        $litop = '<li class="gnav-mitems" aria-expanded="false">';
-        $ulsub = '<ul class="gnav-items-ul" aria-hidden="true">';
-        $lisub = '<li>';
-        $litopalt = '<li class="gnav-toplink">';
-        $ulc = '</ul>';
-        $lic = '</li>';
+        // Finally, build a renderable array from the transformed tree.
+        $menu = $menu_tree->build($tree);
 
-        // CREATE TOP LEVEL UL
-        $newmenu = $ultop;
-
-        // CREATE ELEMENTS
-        foreach ($tree as $item) {
-
-            // TOP LEVEL LINK PARMS
-            $title  = $item->link->getTitle();
-            $url    = $item->link->getUrlObject();
-            $link   = \Drupal\Core\Link::fromTextAndUrl(t($title), $url)->toString();
-
-            if($item->hasChildren){
-
-                // PREASSIGN COUNT TO 1
-                $subcount = 1;
-
-                // BUILD LIST ITEM AND UL SUB ITEM
-                $newmenu .= $litop . $link;
-                $newmenu .= $ulsub;
-
-                foreach ($item->subtree as $subitem){
-
-                    // SUBITEM META
-                    $subtitle   = $subitem->link->getTitle();
-                    $suburl     = $subitem->link->getUrlObject();
-                    $sublink    = \Drupal\Core\Link::fromTextAndUrl(t($subtitle), $suburl)->toString();
-
-                    // ADD TOP LEVEL LINK AS A LINK
-                    if($subcount == 1){
-                        $newmenu .= $litopalt . $link . $lic;
-                    }
-
-                    // CONSTRUCT SUBURL
-                    $newmenu .= $lisub . $sublink . $lic;
-
-                    // INCREMENT SUBCOUNT
-                    $subcount++;
-
-                }
-
-                // CLOSE THE UL AND TOP LEVEL LI ELEMENT
-                $newmenu .= $ulc . $lic;
-
-
-            } else {
-
-                // CREATE A STAND ALONE LINK
-                $newmenu .= $litop . $link . $lic;
-
-            }
-
-        }
-
-        // CLOSE MAIN CONTAINER
-        $newmenu .= $ulc;
-
-        return  array('#markup' => $newmenu);
+        return array('#markup' => drupal_render($menu));
     }
 
 }
