ffa9863594d92792af9d7b66e348adfd1a68e405
John Pardavila <iomaxstudios@hotmail.com>
2017-11-29 08:50:47 -0500

    NDD-1173: Updated code to pre-merge.

--- docroot/profiles/custom/webny/modules/custom/webny_global_nav/src/Twig/webnyGlobalNavBlockExtension.php
+++ docroot/profiles/custom/webny/modules/custom/webny_global_nav/src/Twig/webnyGlobalNavBlockExtension.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Drupal\Core\Link;
 namespace Drupal\webny_global_nav\Twig;
 
 /**
@@ -30,9 +31,6 @@ class webnyGlobalNavBlockExtension extends \Twig_Extension {
      */
     public function globalNavBlock($menu_name, $type, $format) {
         $menu_tree = \Drupal::menuTree();
-
-        // Build the typical default set of menu tree parameters.
-
         $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name)->onlyEnabledLinks()->setMaxDepth(2);
 
         // Load the tree based on this set of parameters.
@@ -40,17 +38,90 @@ class webnyGlobalNavBlockExtension extends \Twig_Extension {
 
         // Transform the tree using the manipulators you want.
         $manipulators = array(
-            // Only show links that are accessible for the current user.
             array('callable' => 'menu.default_tree_manipulators:checkAccess'),
-            // Use the default sorting of menu links.
             array('callable' => 'menu.default_tree_manipulators:generateIndexAndSort'),
         );
+
+        // GET MENU TREE
         $tree = $menu_tree->transform($tree, $manipulators);
 
-        // Finally, build a renderable array from the transformed tree.
-        $menu = $menu_tree->build($tree);
+        // ASSIGN HTML CONST
+        $ultop = '<ul class="gnav-ul">';
+        $litop = '<li class="gnav-mitems gnav-topli" aria-expanded="false">';
+        $ulsub = '<ul class="gnav-items-ul" aria-hidden="true">';
+        $lisub = '<li>';
+        $litopalt = '<li class="gnav-toplink">';
+        $ulc = '</ul>';
+        $lic = '</li>';
 
-        return array('#markup' => drupal_render($menu));
+        // CREATE TOP LEVEL UL
+        $newmenu = $ultop;
+
+        // CREATE ELEMENTS
+        foreach ($tree as $item) {
+
+            // TOP LEVEL LINK PARMS
+            $title  = $item->link->getTitle();
+            $url    = $item->link->getUrlObject();
+            $link   = \Drupal\Core\Link::fromTextAndUrl(t($title), $url)->toString();
+
+            if($item->hasChildren){
+
+                // PREASSIGN COUNT TO 1
+                $subcount = 1;
+                $newmenu .= $litop . $link;
+
+
+                // ENSURE SIZE OF ARRAY BEFORE LOOPING
+                if(sizeof($item->subtree) > 0){
+
+                    $newmenu .= $ulsub;
+
+                    foreach ($item->subtree as $subitem){
+
+                        // SUBITEM META
+                        $subtitle   = $subitem->link->getTitle();
+                        $suburl     = $subitem->link->getUrlObject();
+                        $sublink    = \Drupal\Core\Link::fromTextAndUrl(t($subtitle), $suburl)->toString();
+
+                        // ADD TOP LEVEL LINK AS A LINK
+                        if($subcount == 1){
+
+                            // BUILD LIST ITEM AND UL SUB ITEM
+                            if(!empty($url->toString())){
+                                $newmenu .= $litopalt . $link . $lic;
+                            }
+
+                        }
+
+                        // CONSTRUCT SUBURL
+                        $newmenu .= $lisub . $sublink . $lic;
+
+                        // INCREMENT SUBCOUNT
+                        $subcount++;
+
+                        unset($subitem);
+
+                    }
+
+                    // CLOSE THE UL AND TOP LEVEL LI ELEMENT
+                    $newmenu .= $ulc . $lic;
+
+                };
+
+            } else {
+
+                // CREATE A STAND ALONE LINK
+                $newmenu .= $litop . $link . $lic;
+
+            }
+
+        }
+
+        // CLOSE MAIN CONTAINER
+        $newmenu .= $ulc;
+
+        return  array('#markup' => $newmenu);
     }
 
 }
