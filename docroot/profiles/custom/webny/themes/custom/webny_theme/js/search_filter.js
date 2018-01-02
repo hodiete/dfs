aac658071d9d725ab8afb8dc2df91ce3d8d782dd
Sean Clark <sean.clark@acquia.com>
2018-01-02 14:07:23 -0600

    NDD-1332: add clear link to search views.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/search_filter.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/search_filter.js
@@ -11,6 +11,26 @@
     attach: function (context, settings) {
       // The toggle button.
       var sidebarToggleElement = '.filter-sidebar .filter-toggle';
+      // Clear all results button (see custom 'webny-filter-clear' token).
+      var clearAllButton = '.js-form-clear-all';
+
+      // Reset form within dom.
+      $(clearAllButton, context).on('click', function () {
+        // data-dom-id on 'clear all' element.
+        var current_dom_id = $(this).data('dom-id');
+        var href = window.location.href;
+        var href_no_params = Drupal.Views.getPath(href);
+        var views_settings = Drupal.views.instances['views_dom_id:' + current_dom_id].settings;
+
+        // Ajax settings for view submission.
+        var views_ajax_settings = Drupal.views.instances['views_dom_id:' + current_dom_id].element_settings;
+        views_ajax_settings.submit = views_settings;
+        views_ajax_settings.url = '/views/ajax?q=' + '/' + href_no_params;
+
+        // Submit and clear url parameters.
+        Drupal.ajax(views_ajax_settings).execute();
+        window.history.pushState(null, document.title, href_no_params);
+      });
 
       // Show/hide filter toggle based on viewport width.
       var updateFilter = function() {
