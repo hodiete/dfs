0c67d4644d0328525b9467ede5e975ba3c65cf3e
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2018-01-23 12:22:25 -0500

    NDD-1036: update unav backend js to include search option in display functionality.

--- docroot/profiles/custom/webny/modules/custom/webny_unav/js/webny_unav_backend.js
+++ docroot/profiles/custom/webny/modules/custom/webny_unav/js/webny_unav_backend.js
@@ -17,6 +17,7 @@
       var chk_unav_auto = "input[name='webny_unav_auto']";
       var chk_alt_unav_auto = "input[name='webny_alt_unav_auto']";
       var chk_alt_unav_translate = "input[name='webny_alt_unav_translate']";
+      var chk_alt_unav_search = "input[name='webny_alt_unav_search']";
 
       // FIELDSET
       var fs_alt_unav = "#edit-webny-alt-unav-fieldset";
@@ -35,8 +36,9 @@
         if ($(chk_unav_auto).is(':checked')) {
           $(chk_alt_unav_auto).prop('checked', false);
           $(fs_alt_unav).hide();
-          // REMOVE CHECKED TRANSLATE OPTION IF UNIVERSAL NAV IS SELECTED
+          // REMOVE CHECKED OPTIONS IF UNIVERSAL NAV IS SELECTED
           $(chk_alt_unav_translate).prop('checked', false);
+          $(chk_alt_unav_search).prop('checked', false);
         } else if ($(chk_alt_unav_auto).is(':checked')) {
           $(chk_unav_auto).prop('checked', false);
           $(fs_alt_unav).show();
@@ -44,6 +46,7 @@
           // NO CHECKBOXES ARE CHECKED
           $(fs_alt_unav).hide();
           $(chk_alt_unav_translate).prop('checked', false);
+          $(chk_alt_unav_search).prop('checked', false);
         }
       });
       $(chk_alt_unav_auto).on(click, function() {
@@ -53,12 +56,14 @@
         } else if ($(chk_unav_auto).is(':checked')) {
           $(chk_alt_unav_auto).prop('checked', false);
           $(fs_alt_unav).hide();
-          // REMOVE CHECKED TRANSLATE OPTION IF UNIVERSAL NAV IS SELECTED
+          // REMOVE CHECKED OPTIONS IF UNIVERSAL NAV IS SELECTED
           $(chk_alt_unav_translate).prop('checked', false);
+          $(chk_alt_unav_search).prop('checked', false);
         } else {
           // NO CHECKBOXES ARE CHECKED
           $(fs_alt_unav).hide();
           $(chk_alt_unav_translate).prop('checked', false);
+          $(chk_alt_unav_search).prop('checked', false);
         }
       });
     }
