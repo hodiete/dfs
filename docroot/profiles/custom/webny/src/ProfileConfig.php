6dc713fdb4905db038121f051d37bdac5397055d
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2018-02-14 14:52:12 -0500

    NDD-1036: add webny_unav install directory to ProfileConfig.

--- docroot/profiles/custom/webny/src/ProfileConfig.php
+++ docroot/profiles/custom/webny/src/ProfileConfig.php
@@ -156,6 +156,7 @@ class ProfileConfig {
       $this->getProfilePath() . '/modules/custom/webny_global_nav/' . self::CONFIG_DIR . '/optional',
       $this->getProfilePath() . '/modules/custom/webny_secondary_nav/' . self::CONFIG_DIR . '/install',
       $this->getProfilePath() . '/modules/custom/webny_secondary_nav/' . self::CONFIG_DIR . '/optional',
+      $this->getProfilePath() . '/modules/custom/webny_unav/' . self::CONFIG_DIR . '/install',
       $this->getProfileFeatureConfigPath('base') . '/install',
       $this->getProfileFeatureConfigPath('news_content_type') . '/install',
       $this->getProfileFeatureConfigPath('news_content_type') . '/optional',
