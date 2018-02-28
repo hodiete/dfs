f77225be7b0cc11f916e5600438376cde929133e
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2018-02-28 16:09:13 -0500

    NDD-989: updates to css and js for alternative unav.

--- docroot/profiles/custom/webny/modules/custom/webny_unav/js/webny_alt_unav.js
+++ docroot/profiles/custom/webny/modules/custom/webny_unav/js/webny_alt_unav.js
@@ -15,19 +15,60 @@
       // on click for translate link
       $('#alternative-unav-translate').on(click, function() {
         // set provided google iframe to absolutely positioned
-        $('.goog-te-menu-frame').css('position', 'absolute').css('top', '125px');
+        $('.goog-te-menu-frame').css('position', 'absolute').css('z-index', '100').css('top', '125px');
 
-        // Google sends us three iframes for some reason. Only handle the first instance
-        $('.goog-te-menu-frame').each(function(index, value) {
-          if (index == 0) {
-            if ($(this).css('display') == 'block') {
-              $(this).css('display', 'none');
-            } else {
-              $(this).css('display', 'block');
-            }
+        $('.goog-te-menu-frame:first').toggle();
+
+      });
+
+      $(window).on('load resize', function () {
+        if ($(window).width() < 768) {
+          var windowWidth = $(window).width();
+          var searchbarWidth = windowWidth - 140; // form padding and input padding
+          $('#alternative-unav-search').css('width', searchbarWidth + 'px');
+          $('#alternative-universal-navigation header').css('top', '-84px');
+          $('#expand-menu-mobile').css('top', '94px');
+        } else {
+          $('#alternative-unav-search').css('width', '80px');
+          $('#alternative-universal-navigation header').css('top', '0px');
+        }
+      });
+
+      $('#expand-menu-mobile').on(click, function() {
+        if ($('#alternative-universal-navigation header').css('top') === '-84px') {
+          $('#alternative-universal-navigation header').css('top', '0px');
+          $('.alternative-unav-header').addClass('nav-open');
+
+        } else {
+          $('#alternative-universal-navigation header').css('top', '-84px');
+          $('.alternative-unav-header').removeClass('nav-open');
+        }
+
+      });
+
+      if ($(window).width() >= 768) {
+        $('#alternative-unav-search').on(click, function (e) {
+          e.stopPropagation();
+          if ($('#alternative-unav-search').css('width') == '80px') {
+            var sbMove = $('#alternative-unav-search').offset().left - $('.pane-page-logo').width();
+            var sbWidth = ($('#alternative-unav-search').width() + sbMove);
+
+            console.log(sbMove);
+
+            $('#alternative-unav-search').css('left', -sbMove + 'px').css('width', sbWidth);
+            $('.icon-search').css('left', -(sbMove - 10) + 'px');
+            $('.alternative-unav-gsa form').addClass('search-open');
+            $('.alternative-unav-agency-name').hide();
           }
         });
-      });
+
+        $('body').on(click, function() {
+          $('#alternative-unav-search').css('left', '0px').css('width', '80px');
+          $('.icon-search').css('left', '10px');
+          $('.alternative-unav-gsa form').removeClass('search-open');
+          $('.alternative-unav-agency-name').show();
+        });
+      }
     }
   }
 })(jQuery, Drupal, this);
