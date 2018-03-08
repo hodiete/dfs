761c4b52228e37c3649e9593cde1a6d0566a2035
Lindsey Catlett <lindsey.catlett@gmail.com>
2018-03-08 01:26:41 -0500

    NDD-0: Update Cloud Hooks to use BLT maintained scripts, disable Cloud Hook execution in 01live.

--- factory-hooks/pre-settings-php/includes.php
+++ factory-hooks/pre-settings-php/includes.php
@@ -8,13 +8,6 @@
  */
 
 // Configure your hash salt here.
-$settings['hash_salt'] = 'NZ4pGCYwwI0kPGr5KwDkK1IgC2XOl8jFURj7q8lgAUBw6xxRXlWJNZZ';
+// $settings['hash_salt'] = '';
 
-
-require DRUPAL_ROOT . "/../vendor/acquia/blt/settings/blt.settings.php";
-require DRUPAL_ROOT . '/sites/default/settings/webny.settings.php';
-
-// TEST FOR CLI
-if (php_sapi_name() == "cli") {
-  ini_set('memory_limit', '768M');
-}
+require DRUPAL_ROOT . '/../vendor/acquia/blt/settings/blt.settings.php';
