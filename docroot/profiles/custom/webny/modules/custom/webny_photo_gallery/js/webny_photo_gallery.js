90a9251cec924f1ef9d0bf5f243c51008888fd5c
John Pardavila <iomaxstudios@hotmail.com>
2018-05-03 07:32:50 -0400

    NDD-1480: Updated base JS file for photo gallery before fork from van.

---
+++ docroot/profiles/custom/webny/modules/custom/webny_photo_gallery/js/webny_photo_gallery.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * webny_photo_gallery javascript file for backend
+ */
+
+(function ($, Drupal, window, document) {
+
+  'use strict';
+
+  Drupal.behaviors.photogallery = {
+    attach: function (context, settings) {
+
+    }
+  }
+})(jQuery, Drupal, this);
