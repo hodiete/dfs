79462dee92453e001f64cab02b4ad9f24cceebaa
Chris Catalina <christopher.catalina@its.ny.gov>
2018-06-18 13:51:59 -0400

    NDD-1526: Update webny.settings.php to include the correct seconds conversion to represent 1 year.

--- docroot/sites/default/settings/webny.settings.php
+++ docroot/sites/default/settings/webny.settings.php
@@ -25,14 +25,14 @@ if ($is_local_env) {
  */
 
 # General cache TTL config. Set to 1 year, recommended for Purge and Acquia Purge cache invalidation using Varnish.
-$config['system.performance']['cache']['page']['max_age'] = 31557600;
+$config['system.performance']['cache']['page']['max_age'] = 31536000;
 
 if ($is_ah_env) {
   switch ($ah_env) {
     case '01dev':
       # Disable page cache on dev. Note that this is also set locally in development.services.yml.
       # Set to 1 year for dev Purge and Acquia Purge testing on dev server.
-      $config['system.performance']['cache']['page']['max_age'] = 31557600;
+      $config['system.performance']['cache']['page']['max_age'] = 31536000;
       break;
     case '01live':
       $config['system.logging']['error_level'] = 'hide';
