092936d0c2ae3c1bab3b7ab64308d42f0ed8506f
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2018-06-27 10:59:34 -0400

    NDD-1541: need to kick off travis.

--- docroot/profiles/custom/webny/themes/custom/webny_theme/js/navigator.js
+++ docroot/profiles/custom/webny/themes/custom/webny_theme/js/navigator.js
@@ -104,7 +104,7 @@
         }
 
         // check if the next section has an id attribute indicating there is a next section
-        // previously we were checking for text in the title which doesn't translate to all paragraph types
+        // previously we were checking for text in the title which doesn't translate to all paragraph types 
         if (nextSection.attr('id')) {
           if ($(nextSection).hasClass('webny_wysiwyg_pgtype')) {
             $(this).children('.next-section').children('.next-section-title').html(nextSection.children(':first').text());
