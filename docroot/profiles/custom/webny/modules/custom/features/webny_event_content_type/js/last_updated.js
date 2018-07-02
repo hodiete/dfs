82f35f1671076363c829357a4f3e29fbef79d3bf
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2018-07-02 09:44:17 -0400

    NDD-1519: update functionality of time toggle with js.

--- docroot/profiles/custom/webny/modules/custom/features/webny_event_content_type/js/last_updated.js
+++ docroot/profiles/custom/webny/modules/custom/features/webny_event_content_type/js/last_updated.js
@@ -6,19 +6,20 @@
       var lastupdated_time = '#edit-field-webny-event-last-updated-0-value-time';
       var lastupdated_checkbox = '#edit-field-webny-event-upd-time-value';
 
-      if ($(lastupdated_checkbox).checked) {
+      if ($(lastupdated_time).val() === '') {
+        $(lastupdated_time).val('00:00:00');
+      }
+
+      // on load display the time textbox
+      if ($(lastupdated_checkbox).is(':checked')) {
         $(lastupdated_time).css('display', 'block');
       } else {
         $(lastupdated_time).css('display', 'none');
       }
 
+      // if checked, toggle the display of the time textbox
       $(lastupdated_checkbox).change(function () {
         $(lastupdated_time).toggle();
-        /*if ($(lastupdated_checkbox).checked) {
-          $(lastupdated_time).css('display', 'block');
-        } else {
-          $(lastupdated_time).css('display', 'none');
-        }*/
       });
     }
   };
