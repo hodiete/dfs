77adf2ce5df9fa9a3bd6d9c3c8f237abd6b28b6f
Christofer VanWormer <christofer.vanwormer1@its.ny.gov>
2018-07-11 13:20:09 -0400

    NDD-1519: prepend #last-updated with help text.

--- docroot/profiles/custom/webny/modules/custom/features/webny_event_content_type/js/last_updated.js
+++ docroot/profiles/custom/webny/modules/custom/features/webny_event_content_type/js/last_updated.js
@@ -6,6 +6,8 @@
       var lastupdated_time = '#edit-field-webny-event-last-updated-0-value-time';
       var lastupdated_checkbox = '#edit-field-webny-event-upd-time-value';
 
+      $('#last-updated').prepend('<p style="padding:0 1.5em;">Ability to indicate when content was last updated (optional).</p>');
+
       // if time isn't set, set a default of midnight to pass form validation
       if ($(lastupdated_time).val() === '' && $(lastupdated_date).val() !== '') {
         $(lastupdated_time).val('00:00:00');
