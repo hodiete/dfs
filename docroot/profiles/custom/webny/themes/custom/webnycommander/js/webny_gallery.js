053cdf7f9c215727954b0b3865e46e0874c23338
John Pardavila <iomaxstudios@hotmail.com>
2018-07-19 18:25:02 -0400

    NDD-1543: Added changes to js and css to make dynamic descriptions. WIP.

--- docroot/profiles/custom/webny/themes/custom/webnycommander/js/webny_gallery.js
+++ docroot/profiles/custom/webny/themes/custom/webnycommander/js/webny_gallery.js
@@ -15,6 +15,11 @@ var gallery_backend = {
   gallery_order: '.field--name-field-webny-gallery-images .gallery-img-order',
   gallery_description_item: '.field--name-field-webny-gallery-image-desc table.field-multiple-table tbody tr.draggable',
   gallery_description_handle:   '.field--name-field-webny-gallery-image-desc .field-multiple-table tbody tr.draggable',
+  photo_number_down: null,
+  photo_number_up: null,
+  finalIndex: null,
+  startIndex: null,
+  gallerySize: null,
 
   // IMAGE TITLE PREPENDER
   prepend_image_title: function(number){
@@ -51,7 +56,7 @@ var gallery_backend = {
     for(var i = 1; i <= $(gallery_backend.gallery_item).length; i++) {
 
       $('.field--name-field-webny-gallery-images .entities-list .item-container:nth-child('+i+') .gallery-img-order')
-        .html('<b>Gallery Image '+i+'</b>').hide().fadeIn(150);
+      .html('<b>Gallery Image '+i+'</b>').hide().fadeIn(150);
 
       gallery_backend.order[i] = $('.field--name-field-webny-gallery-images .entities-list .item-container:nth-child('+i+')').attr('data-row-id');
 
@@ -89,17 +94,56 @@ var gallery_backend = {
    */
   reorder: function() {
 
-    var current_handle = '';
-    var current_tr = [];
+    // VARS
+    var new_des = null;
+    var ori_loc = null;
 
-    // DO THE REORDER
-    for(var x = 1; x < gallery_backend.order.length; ++x) {
+    var fI = gallery_backend.finalIndex + 1;
+    var sI = gallery_backend.startIndex;
+    var gS = gallery_backend.gallerySize;
 
-      current_handle = gallery_backend.gallery_description_handle;
-      current_handle += ':nth-child('+x+')';
-      current_tr[x] = $(current_handle).html();
 
+    console.log('GAL Size: ' + gallery_backend.gallerySize);
+    console.log('Original: ' + sI);
+    console.log('Final:    ' + fI);
+
+
+    // MAKE THE MOVE
+    if(parseInt(fI) === parseInt(gS)){
+
+      // GET DESCRIPTIONS
+      new_des = gallery_backend.gallery_description_handle + ':last-child()';
+      ori_loc = gallery_backend.gallery_description_handle + ':nth-child('+ sI +')';
+
+      $(ori_loc).insertAfter(new_des);
+      console.log('AFTER');
+    } else {
+
+
+      // GET DESCRIPTIONS
+      new_des = gallery_backend.gallery_description_handle + ':nth-child('+ fI +')';
+      ori_loc = gallery_backend.gallery_description_handle + ':nth-child('+ sI +')';
+
+      $(ori_loc).insertBefore(new_des);
+      console.log('BEFORE');
     }
+
+
+  },
+
+  finalIndexProc: function(tI) {
+
+    // IF DN > UP, Dragged LEFT [0-9]
+    // PH is a final location
+
+    // IF ===, dragged RIGHT [0-9]
+    // PH - 1 is final index location
+
+    if( gallery_backend.photo_number_down === gallery_backend.photo_number_up){
+      tI -= 1;
+    }
+
+    return tI;
 
   }
 
@@ -116,17 +160,21 @@ var gallery_backend = {
   Drupal.behaviors.videohero = {
     attach: function (context, settings) {
 
+      var isDragging = false;
+      var wasDragging = false;
+      var thisIndex = false;
+
+
       // ORDER THE GALLERY PROPERLY
       gallery_backend.photo_order();
 
-
-      // REORDER AFTER CLICK UP
-      $(gallery_backend.gallery_item).on('mouseup', function (){
-        setTimeout(function () {
-          gallery_backend.photo_reorder();
-        }, 100);
+      // ADD ID ONLOAD OF THE GALLERY
+      $(gallery_backend.gallery_description_item).each(function(i){
+        //$(this).attr('data-gallery-desc-id',i);
+        //$(this).attr('data-gallery-desc-order',i);
       });
 
+      // CLICK
       $(gallery_backend.gallery_description_handle).on('click', function (e){
 
         setTimeout(function () {
@@ -134,6 +182,55 @@ var gallery_backend = {
         }, 100);
 
       });
+
+      // REORDER AFTER CLICK UP
+      $(gallery_backend.gallery_item).mousedown(function() {
+
+        // SET DRAGGING AS FALSE
+        isDragging = false;
+
+        // SIZE OF GALLERY
+        gallery_backend.gallerySize = $(gallery_backend.gallery_item).length;
+
+        // CHECK THE INDEX OF THE PHOTO
+        gallery_backend.photo_number_down = parseInt($(this).index());
+
+      }).mousemove(function(){
+
+        // SET AS DRAGGING
+        isDragging = true;
+        thisIndex = $('.ui-sortable-placeholder').index();
+
+      }).mouseup(function (){
+
+        // DETERMINE IF DRAGGING
+        wasDragging = isDragging;
+
+        // RESET DRAGGING TO FALSE
+        isDragging = false;
+
+        gallery_backend.photo_number_up = parseInt($(this).index());
+
+        // IF DRAGGED, CONSIDER THE PLACE HOLDER FOR THE PHOTOS
+        if (wasDragging && thisIndex !== -1) {
+
+          // CALCULATE START INDEX OF THE PHOTO
+          gallery_backend.startIndex = gallery_backend.photo_number_down;
+
+          // CALCULATE FINAL INDEX LOCATION OF THE PHOTO
+          gallery_backend.finalIndex = gallery_backend.finalIndexProc(thisIndex);
+
+          // REORDER ALL THE THINGS
+          setTimeout(function () {
+            gallery_backend.photo_reorder();
+            gallery_backend.reorder();
+          }, 50);
+        }
+      });
+
+
+
+
     },
     detach: function () {}
 
@@ -143,5 +240,9 @@ var gallery_backend = {
 
 // ONLOAD
 $(function(){
+
+  // ORDER ONLOAD
   gallery_backend.description_order();
+
 });
+
