bb0797b1e5bebff97db79bf8c7483cd2ef2ef740
Ryan Grainer <ryan.grainer@its.ny.gov>
2018-08-01 15:40:56 -0400

    NDD-1529: adding application name to new relic hook.

--- factory-hooks/post-settings-php/new_relic.php
+++ factory-hooks/post-settings-php/new_relic.php
@@ -2,5 +2,5 @@
 if (extension_loaded('newrelic')) {
     $domain_fragments = explode('.', $_SERVER['HTTP_HOST']);
     $site_name = array_shift($domain_fragments);
-    newrelic_set_appname("$site_name;webny_distro", '', 'true');
+    newrelic_set_appname("$site_name;nysits.01live", '', 'true');
 }
