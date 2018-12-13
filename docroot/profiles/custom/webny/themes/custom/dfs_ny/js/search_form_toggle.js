464c6c3125f2ebbe80a565ee94c49d9a9ae248e0
gungwang <wghappytin@hotmail.com>
2018-12-13 17:24:59 -0500

    Gung: portal log button css and js

--- docroot/profiles/custom/webny/themes/custom/dfs_ny/js/search_form_toggle.js
+++ docroot/profiles/custom/webny/themes/custom/dfs_ny/js/search_form_toggle.js
@@ -43,54 +43,7 @@
         $(sidebarToggleElement).css('marginRight', rightDistance + 'px');
       });
       
-      /*
-      // Reset form within dom.
-      $(clearAllButton, context).on('click', function () {
-        // data-dom-id on 'clear all' element.
-        var current_dom_id = $(this).data('dom-id');
-        var href = window.location.href;
-        var href_no_params = Drupal.Views.getPath(href);
-        var views_settings = Drupal.views.instances['views_dom_id:' + current_dom_id].settings;
-
-        // Ajax settings for view submission.
-        var views_ajax_settings = Drupal.views.instances['views_dom_id:' + current_dom_id].element_settings;
-        views_ajax_settings.submit = views_settings;
-        views_ajax_settings.url = '/views/ajax?q=' + '/' + href_no_params;
-
-        // Submit and clear url parameters.
-        Drupal.ajax(views_ajax_settings).execute();
-        window.history.pushState(null, document.title, '/' + href_no_params);
-      });
-
-      // set filtertoggle based on viewportwidth
-      if ($(window).width() < 1024) {
-        $(sidebarToggleElement).removeClass('hidden');
-      }
-      else {
-        $(sidebarToggleElement).addClass('hidden');
-      }
-
-      // Show/hide filter toggle based on viewport width.
-      var updateFilter = function () {
-        var viewportWidth = $(window).width();
-
-        if (viewportWidth < 1024) {
-          $(sidebarToggleElement).removeClass('hidden');
-        }
-        else {
-          $(sidebarToggleElement).addClass('hidden');
-        }
-      };
-
-      // Toggle expanded class to hide/show contents via css.
-      $(sidebarToggleElement, context).on('click', function () {
-        $(this).parent().toggleClass('expanded');
-      });
-
-      // Update on resize.
-      $(window).on('resize', debounce(updateFilter, 200));
-
-      */
+     
     }
   };
 
@@ -111,4 +64,30 @@
     }
   };
 
+  Drupal.behaviors.portalLogin = {
+    attach: function (context, settings) {
+      // The toggle button.
+      var portal = '.webny-global-header ul.gnav-ul li.gnav-topli:nth-child(7) > a';
+      var button = '.login-button';
+      // var iframe = '.portal-wrapper iframe';
+      // var close = '.portal-wrapper .close';
+      // Clear all results button (see custom 'webny-filter-clear' token).
+      console.log(portal);
+      $(portal).click(function () {
+        window.open(this.href);
+        return false;
+      });
+      // $(portal).click(function (e) {
+      //   console.log($(this).attr('href'));
+      //   e.preventDefault();
+      //   $(iframe).attr("src", $(this).attr('href'));
+      //   $(wrapper).fadeIn('slow');
+
+      // });
+      // $(close).click(function () {
+      //   $(this).parent().fadeOut("slow");
+      // });
+    }
+  };
+  
 })(jQuery, Drupal, Drupal.debounce);
