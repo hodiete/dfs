3fbc8d06e77a5243fcce0a42c4b194fc1330f393
gungwang <wghappytin@hotmail.com>
2019-01-03 13:46:17 -0500

    Gung: settings.php

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -535,7 +535,7 @@ $settings['file_public_path'] = 'sites/default/files';
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-$settings['file_private_path'] = 'sites/default/files/private';
+# $settings['file_private_path'] = '';
 
 /**
  * Session write interval:
