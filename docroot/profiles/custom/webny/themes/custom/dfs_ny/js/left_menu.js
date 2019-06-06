c69636203370cfed44fe79df2adee7cde58c6f21
Henry Odiete <henry.odiete@opin.ca>
2019-06-06 11:20:34 -0400

    Left Hand side nagivation modified to include sub menu items

--- docroot/profiles/custom/webny/themes/custom/dfs_ny/js/left_menu.js
+++ docroot/profiles/custom/webny/themes/custom/dfs_ny/js/left_menu.js
@@ -92,9 +92,9 @@
 
       let generalSidebarHeight = $sticky.innerHeight();
       let stickyTop = $sticky.offset().top;
-      if ($sticky.lenght) {
-        // stickyTop = $sticky.offset().top;
-      }
+      // if ($sticky.length) {
+      //   // stickyTop = $sticky.offset().top;
+      // }
    
       let stickOffset = 0;
       let stickyStopperPosition = 0;
@@ -113,7 +113,9 @@
       let diff = stopPoint + stickOffset;     
 
       $(window).scroll(function() { // scroll event
-   
+      generalSidebarHeight = $sticky.innerHeight();
+      stopPoint = stickyStopperPosition-generalSidebarHeight;
+      diff = stopPoint + stickOffset; 
         var windowTop = $(window).scrollTop(); // returns number    
         console.log('windowTop = ' + windowTop + 'stopTop = ' +stopPoint );
         if ($topMenu && windowTop <= 90) {
